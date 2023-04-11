a590b6391a8ea53aa698a3a13d084163bbe53258
Yojhan <yojhansg@ucm.es>
2023-04-11 17:36:43 +0200

    eventos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// This file is part of the course TPV2@UCM - Samir Genaim
-
 #include <iostream>
 #include <memory>
 
@@ -24,17 +22,17 @@ int main(int, char**) {
 	//memory leaks
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	try {
+	//try {
 		start();
-	} catch (const std::string &e) { // catch exceptions thrown as strings
-		std::cerr << e << std::endl;
-	} catch (const char *e) { // catch exceptions thrown as char*
-		std::cerr << e << std::endl;
-	} catch (const std::exception &e) { // catch exceptions thrown as a sub-type of std::exception
-		std::cerr << e.what();
-	} catch (...) {
-		std::cerr << "Caught and exception of unknown type ...";
-	}
+	//} catch (const std::string &e) { // catch exceptions thrown as strings
+	//	std::cerr << e << std::endl;
+	//} catch (const char *e) { // catch exceptions thrown as char*
+	//	std::cerr << e << std::endl;
+	//} catch (const std::exception &e) { // catch exceptions thrown as a sub-type of std::exception
+	//	std::cerr << e.what();
+	//} catch (...) {
+	//	std::cerr << "Caught and exception of unknown type ...";
+	//}
 
 	return 0;
 }
