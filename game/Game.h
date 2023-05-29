67e01661a15a945ef161c667627b650fbf5b7ef3
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2023-05-29 15:01:59 +0200

    Comenzando con la correción

--- game/Game.h
+++ game/Game.h
@@ -1,15 +1,14 @@
-// This file is part of the course TPV2@UCM - Samir Genaim
-
 #pragma once
+
 #include "../classes/countdown.h"
 #include "../classes/game_scene.h"
 #include "../classes/game_state_machine.h"
 #include "../utils/checkML.h"
+#include "../classes/player_save_data.h"
+
 #include <memory>
 #include <stack>
-#include "../classes/player_save_data.h"
-// when you are not using the methods of a class, just
-// say that it exists, that saves time when parsing files
+
 class Manager;
 class Entity;
 
