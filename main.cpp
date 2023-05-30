fe6d7bbb532fec0e96a734739b450a73aa3c5938
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2023-05-30 15:50:27 +0200

    Git ignora el directorio con los eventos generados

--- main.cpp
+++ main.cpp
@@ -19,20 +19,10 @@ void start() {
 }
 
 int main(int, char**) {
-	//memory leaks
+
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	//try {
-		start();
-	//} catch (const std::string &e) { // catch exceptions thrown as strings
-	//	std::cerr << e << std::endl;
-	//} catch (const char *e) { // catch exceptions thrown as char*
-	//	std::cerr << e << std::endl;
-	//} catch (const std::exception &e) { // catch exceptions thrown as a sub-type of std::exception
-	//	std::cerr << e.what();
-	//} catch (...) {
-	//	std::cerr << "Caught and exception of unknown type ...";
-	//}
+	start();
 
 	return 0;
 }
